cb27352c715e390be4983dce2384fc43d4f41965
NandhiniM1903 <100334670+NandhiniM1903@users.noreply.github.com>
2022-06-07 11:21:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GroceryDelivery
+ Final Project
---
+++ src/test/java/com/edu/GroceryDelivery/GroceryDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.GroceryDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
